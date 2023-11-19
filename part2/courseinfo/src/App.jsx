3ea2e91e0bcfc16af4e868e97a26f4b2edaa4c4b
unknown <rogeliomdn@gmail.com>
2023-11-19 22:03:34 +0800

    Exercise 2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,6 +1,16 @@
 const Header = ({ text }) => <h1>{text}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Total = ({ course }) => {
+  let total = 0;
+  course.parts.forEach(part => {
+    total += part.exercises
+  });
+
+  return (
+    <p><b>Total of {total} exercises</b></p>
+  )
+}
 
 const Part = ({ part }) =>
   <p>
@@ -16,6 +26,7 @@ const Course = ({ course }) =>
   <>
     <Header text={course.name} />
     <Content parts={course.parts} />
+    <Total course={course} />
   </>
 
 const App = () => {
@@ -37,6 +48,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }

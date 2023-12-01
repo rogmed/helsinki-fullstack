fa6412f383eb8542a6c3bfb47c63b93cb3050cb8
unknown <rogeliomdn@gmail.com>
2023-12-01 11:58:55 +0800

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const mostVotedBlog = blogs.reduce((mostVoted, blog) =>
+        mostVoted.likes > blog.likes ? mostVoted : blog)
+
+    return {
+        "author": mostVotedBlog.author,
+        "likes": mostVotedBlog.likes,
+        "title": mostVotedBlog.title
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

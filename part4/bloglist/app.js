165346bc64967219c0d8d735732dd32d376f2604
unknown <rogeliomdn@gmail.com>
2023-12-07 23:17:18 +0800

    Exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set("bufferTimeoutMS", 30000)
 mongoose.connect(config.MONGODB_URI)
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
